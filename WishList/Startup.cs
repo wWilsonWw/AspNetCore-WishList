f29d116c271b5370994c95f93b8f81810ffd7a67
wWilsonWw <golubkin.timofey@yandex.ru>
2021-07-21 14:38:53 +0100

    Add middelware

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -17,14 +17,18 @@ namespace WishList
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.Run(async (context) =>
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
